d3f001704f2b05b6313c8166813759c5f44cf80e
cruazer@gmail.com <cruazer@gmail.com@2fec6651-96fc-3298-734f-5bea4bee4f3c>
2012-10-22 15:19:47 +0000

    RIS Lab03, Lab03.Individual, Kot03.Individual

---
+++ RIS/Lab03/Kot03.Individual/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace Kot03.Individual
+{
+	static class Program
+	{
+		private const string File1 = @"..\..\..\..\file1.txt";
+		private const string File2 = @"..\..\..\..\file2.txt";
+		private const string File3 = @"..\..\..\..\file3.txt";
+
+		static void Main(string[] args)
+		{
+			int tokenLength;
+			string originalText;
+
+			if (!File.Exists(File1))
+			{
+				Console.WriteLine("No file file1.txt");
+				return;
+			}
+
+			Console.WriteLine("Reading file1.txt");
+			using (var sr = new StreamReader(File1))
+			{
+				originalText = sr.ReadToEnd();
+			}
+
+			Console.WriteLine(Environment.NewLine);
+			Console.WriteLine("Original Text: {0}", originalText);
+
+			Console.WriteLine(Environment.NewLine);
+			Console.Write("Enter token length: ");
+			if (!int.TryParse(Console.ReadLine(), out tokenLength))
+			{
+				Console.WriteLine("Not a number");
+				return;
+			}
+
+			var modifiedText = Regex.Replace(originalText,
+				string.Format(@"\b[bcdfghjklmnpqrstvwxzBCDFGHJKLMNPQRSTVWXZ]{{1}}\w{{{0}}}\b", tokenLength - 1), "");
+
+			Console.WriteLine(Environment.NewLine);
+			Console.WriteLine("Modified Text: {0}", modifiedText);
+
+			Console.WriteLine(Environment.NewLine);
+			Console.WriteLine("Writing file2.txt");
+			using (var sr = new StreamWriter(File2))
+			{
+				sr.Write(modifiedText);
+			}
+
+			Console.WriteLine(Environment.NewLine);
+			Console.WriteLine("Writing file3.txt");
+			using (var sr = new StreamWriter(File3))
+			{
+				sr.WriteLine(originalText);
+				sr.Write(Environment.NewLine);
+				sr.Write(modifiedText);
+			}
+		}
+	}
+}
---
+++ RIS/Lab03/Lab03.Individual/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Lab03.Individual
+{
+	static class Program
+	{
+		private const string File1 = @"..\..\..\..\file1.txt";
+		private const string File2 = @"..\..\..\..\file2.txt";
+		private const string File3 = @"..\..\..\..\file3.txt";
+
+		private static readonly char[] Vowels = new[] { 'e', 'y', 'u', 'i', 'o', 'a' };
+
+		static void Main(string[] args)
+		{
+			string originalText;
+
+			if (!File.Exists(File1))
+			{
+				Console.WriteLine("No file file1.txt");
+				return;
+			}
+
+			Console.WriteLine("Reading file1.txt");
+			using (var sr = new StreamReader(File1))
+			{
+				originalText = sr.ReadToEnd();
+			}
+
+			int totalCount = 0, spacesCount = 0, vowelsCount = 0;
+
+			foreach (var x in originalText.ToCharArray())
+			{
+				totalCount++;
+				if (x == ' ')
+					spacesCount++;
+				else if (Vowels.Contains(x))
+					vowelsCount++;
+			}
+
+			var result = new StringBuilder()
+				.AppendFormat("Total symbols count: {0}", totalCount)
+				.Append(Environment.NewLine)
+				.AppendFormat("Vowels count: {0}", vowelsCount)
+				.Append(Environment.NewLine)
+				.AppendFormat("Whitespaces count: {0}", spacesCount)
+				.ToString();
+
+			Console.WriteLine("Writing file2.txt");
+			using (var sr = new StreamWriter(File2))
+			{
+				sr.Write(result);
+			}
+
+			Console.WriteLine("Writing file3.txt");
+			using (var sr = new StreamWriter(File3))
+			{
+				sr.WriteLine(originalText);
+				sr.Write(Environment.NewLine);
+				sr.Write(result);
+			}
+		}
+	}
+}
