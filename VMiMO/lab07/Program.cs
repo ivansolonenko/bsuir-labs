2d2f05a4355fabbe490efa3593f4ec3806690ee7
cruazer@gmail.com <cruazer@gmail.com@2fec6651-96fc-3298-734f-5bea4bee4f3c>
2012-12-05 21:28:21 +0000

    VMiMO Lab07, Lab08

---
+++ VMiMO/lab07/Program.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using labs.Helpers;
+
+namespace lab07
+{
+	static class Program
+	{
+		const int N = 3;
+		const double E = 0.001;
+		const double EE = 0.00001;
+		const double D_SOURCE = 10;
+		static readonly double GOLDEN = (3 - Math.Sqrt(5)) / 2;
+		static double[] D;
+
+		static double Func(double[] x)
+		{
+			var theta = Math.Atan(x[1] / x[0]);
+			var r = Math.Sqrt(Math.Pow(x[0], 2) + Math.Pow(x[1], 2));
+			return 100 * ((x[2] - 10 * theta) * (x[2] - 10 * theta) + (r - 1) * (r - 1)) + x[2] * x[2] * x[2];
+			//return 100 * (x[0] - 1) * (x[0] - 1) + (x[1] - 2) * (x[1] - 2) + (x[2] - 3) * (x[2] - 3);
+		}
+
+		static bool IsMin()
+		{
+			for (var i = 0; i < N; ++i)
+				if (D[i] > E)
+					return false;
+			return true;
+		}
+
+		static void Calc(double[] x0)
+		{
+			do
+			{
+				var m = 0;
+				for (var i = 0; i < N; ++i)
+				{
+					var x = (double[])x0.Clone();
+					x[i] += D[i];
+					if (Func(x) < Func(x0))
+					{
+						x0[i] = x[i];
+					}
+					else
+					{
+						x[i] = x0[i] - D[i];
+						if (Func(x) < Func(x0))
+							x0[i] = x[i];
+						else
+							++m;
+					}
+				}
+				if (N == m)
+					for (var i = 0; i < N; ++i)
+						D[i] = D[i] / 2;
+			} while (!IsMin());
+		}
+
+		static void Golden(double[] x0)
+		{
+			for (var k = 0; k < N; ++k)
+			{
+				var a = x0[k] - E;
+				var b = x0[k] + E;
+				var x1 = (double[])x0.Clone();
+				while (Math.Abs(b - a) > EE)
+				{
+					x0[k] = a + GOLDEN * (b - a);
+					x1[k] = b - GOLDEN * (b - a);
+					if (Func(x0) > Func(x1))
+						a = x0[k];
+					else
+						b = x1[k];
+				}
+			}
+		}
+
+		// ReSharper disable UnusedParameter.Local
+		static void Main(string[] args)
+		// ReSharper restore UnusedParameter.Local
+		{
+			var x0 = new double[N];
+
+			Console.WriteLine("Начальная точка: ");
+			for (var i = 0; i < N; ++i)
+			{
+				Console.Write("X[" + i + "] = ");
+				x0[i] = Helpers.StringToNumber<double>(Console.ReadLine());
+			}
+
+			D = new double[N];
+			Console.Write("Начальный вектор: (");
+			for (var i = 0; i < N; ++i)
+			{
+				D[i] = D_SOURCE;
+				Console.Write(D[i] + " ");
+			}
+			Console.WriteLine(")");
+
+			Calc(x0);
+			Golden(x0);
+
+			Console.Write("Результат: (");
+			for (var i = 0; i < N; ++i)
+				Console.Write(x0[i] + " ");
+			Console.WriteLine(")");
+		}
+	}
+}
